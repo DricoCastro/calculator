b69b2011307a0e87d8cbbf93dc7be7f216c7e6eb
DricoCastro <adrianosantosdoco@gmail.com>
2021-11-30 17:01:31 -0300

    change name appp and color

--- lib/components/button.dart
+++ lib/components/button.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 class Button extends StatelessWidget {
   static const colorDark = Color.fromRGBO(82, 82, 82, 1);
   static const colorDefault = Color.fromRGBO(112, 112, 112, 1);
-  static const colorOperation = Color.fromRGBO(250, 158, 13, 1);
+  static const colorOperation = Color.fromRGBO(95, 195, 20, 1);
 
   final String text;
   final bool big;
